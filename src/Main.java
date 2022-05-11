92997c39c9afa1109a27e58844bc34aec4bca8b7
Hovhannes <hovnaz.arm@gmail.com>
2022-05-11 18:27:10 +0400

    figure homework

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
-import main.library.tree.BinaryTree;
+import homework.figurePoint.Figure;
 
 public class Main {
 
     public static void main(String[] args) {
-//        BinaryTree tree = new BinaryTree();
-//        tree.add(5);
-//        tree.add(4);
-//        tree.add(6);
-//        tree.add(45);
-//        tree.add(49);
-//        tree.print();
+        new Figure();
     }
 }
