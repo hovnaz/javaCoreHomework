114a2e3507e059725a717cd9914dffeb4f44aa8c
hovnaz <hovnaz.arm@gmail.com>
2022-05-15 18:09:51 +0400

    add sum and arithmeticMean method

--- src/homework/arrayutil/ArrayUtil.java
+++ src/homework/arrayutil/ArrayUtil.java
@@ -4,6 +4,21 @@ import java.util.Arrays;
 
 public class ArrayUtil {
     int[] data;
+
+    public static void main(String[] args) {
+        int[] array = {2,5,8,10,32,22,11,66,12,30};
+        ArrayUtil list = new ArrayUtil(array);
+        System.out.println("max "+list.max());
+        System.out.println("min "+list.min());
+        System.out.println("even "+Arrays.toString(list.even()));
+        System.out.println("odd"+Arrays.toString(list.odd()));
+        System.out.println("evenCount "+list.evenCount());
+        System.out.println("oddCount "+ list.oddCount());
+        System.out.println(list.sum());
+        System.out.println(list.arithmeticMean());
+        list.print();
+    }
+
     public ArrayUtil(int[] arr){
         data = arr;
     }
@@ -15,6 +30,16 @@ public class ArrayUtil {
         }
         System.out.print("]");
 
+    }
+    public long sum(){
+       long res = 0;
+        for (int i = 0; i < data.length; i++) {
+            res+=data[i];
+        }
+        return res;
+    }
+    public int arithmeticMean(){
+        return (int)(sum() / data.length);
     }
     public int max(){
         int res = data[0];
