2945cd539255d0f33e3c463e0c3a53ff25d05df1
Hovhannes <hovnaz.arm@gmail.com>
2022-07-31 11:42:04 +0400

    CollectionExample done

--- src/homework/collection/CollectionExample.java
+++ src/homework/collection/CollectionExample.java
@@ -1,8 +1,6 @@
 package homework.collection;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class CollectionExample {
 
@@ -12,37 +10,42 @@ public class CollectionExample {
 
     //մեթոդը գրել այնպես, որ կանչենք, ու իրան ստրինգների մասիվ տանք, ինքը վերադարձնի նույն էլեմենտներով ArrayList
     static List<String> arrayToList(String[] values) {
-        return null;
+        return new ArrayList<String>(List.of(values));
     }
 
     //մեթոդը գրել այնպես, որ կանչենք ու տանք ինչ որ լիստ, ու ինդեքս, ինքը ջնջե էդ ինդեքսի տակ գտնվող էլեմենտը, ու վերադարձնե նույն լիստը, առանց էդ էլեմենտի։
     static List<String> removeById(List<String> list, int index) {
-        return null;
+        list.remove(index);
+        return list;
     }
 
     // մեթոդը գրել այնպես, որ ընդունի ցանկացած String-ների լիստ, վերադարձնի նույն արժեքներով HashSet
     static Set<String> listToSet(List<String> list) {
-        return null;
+        return new HashSet<>(list);
     }
 
     // մեթոդը գրել այնպես, որ ընդունի ցանկացած Integer-ների լիստ, վերադարձնի նույն արժեքներով LinkedList-ի օբյեկտ։
     static LinkedList<Integer> listToLinkedList(List<Integer> list) {
-        return null;
+        return new LinkedList<>(list);
     }
 
     // մեթոդը գրել այնպես, որ ընդունի ցանկացած Integer-ների լիստ, վերադարձնի նոր լիստ, որի մեջ էլեմենտները կլինեն հակառակ հերթականությամբ։
     static List<Integer> reverseNumbers(List<Integer> list) {
-        return null;
+        Collections.reverse(list);
+        return list;
     }
 
     // մեթոդը գրել այնպես, որ ինչ մեծությամբ լիստ էլ տանք էս մեթոդին, ինքը տպե մենակ սկզբի 5 հատը։
     static void printFirstFive(List<String> list) {
-
+        int len = Math.min(list.size(), 5);
+        for (int i = 0; i < len; i++) {
+            System.out.print(list.get(i) + " ");
+        }
     }
 
     // մեթոդը գրել այնպես, որ ինչ list տանք վերադարձնի թե քանի էլեմենտ կա էդ լիստ-ի մեջ
     static int sizeOfList(List<String> list) {
-        return 0;
+        return list.size();
     }
 
 }
